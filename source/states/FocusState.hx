0bd8101dd45287b4627a6a4e6a8b8629fffe2427
steverichey <stevenpatrickrichey@gmail.com>
2014-01-22 22:23:32 -0600

    update states, preloader, assets, convert wav to ogg

--- source/states/FocusState.hx
+++ source/states/FocusState.hx
@@ -1,20 +1,13 @@
-package;
+package states;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
-import flixel.ui.FlxButton;
-import flixel.util.FlxMath;
 
-/**
- * A FlxState which can be used for the actual gameplay.
- */
-class PlayState extends FlxState
+class FocusState extends FlxState
 {
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
+	private var _clickHere:FlxSprite;
+	
 	override public function create():Void
 	{
 		// Set a background color
@@ -24,23 +17,27 @@ class PlayState extends FlxState
 		FlxG.mouse.show();
 		#end
 		
+		_clickHere = new FlxSprite( 0, 0, "images/clickhere.png" );
+		
+		add( _clickHere );
+		
 		super.create();
 	}
 	
-	/**
-	 * Function that is called when this state is destroyed - you might want to 
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
-	override public function destroy():Void
-	{
-		super.destroy();
-	}
-
-	/**
-	 * Function that is called once every frame.
-	 */
 	override public function update():Void
 	{
+		#if !FLX_NO_MOUSE
+		if ( FlxG.mouse.justPressed ) {
+			FlxG.switchState( new PlayState() );
+		}
+		#end
+		
 		super.update();
-	}	
+	}
+	
+	override public function destroy():Void
+	{
+		_clickHere = null;
+		super.destroy();
+	}
 }
