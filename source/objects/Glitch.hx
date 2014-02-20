e0b7e487a10e174de5edc3ad6caaa3542f4b6821
steverichey <stevenpatrickrichey@gmail.com>
2014-02-19 18:18:16 -0600

    remove ugly glitches

--- source/objects/Glitch.hx
+++ source/objects/Glitch.hx
@@ -4,62 +4,58 @@ import flash.Vector;
 import flash.geom.Rectangle;
 import flixel.FlxBasic;
 import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.util.FlxRandom;
 
+/**
+ * A glitch object, which applies itself directly to the flixel camera every frame.
+ */
 class Glitch extends FlxBasic
 {
-	private var _x:Float = 0;
-	private var _y:Float = 0;
-	private var _width:Float = 0;
-	private var _height:Float = 0;
+	private var _width:Int = 0;
+	private var _height:Int = 0;
 	private var _dead:Bool = false;
-	private var _once:Bool = false;
-	private var _type:Int = 0;
 	private var _rectangle:Rectangle;
 	private var _randomRectangle:Rectangle;
-	private var _hospitalWithout:FlxSprite;
 	private var _vector:Vector<UInt>;
 	
 	public function new()
 	{
 		super();
 		
-		_x = FlxRandom.intRanged( 0, FlxG.width );
-		_y = FlxRandom.intRanged( 0, FlxG.height );
+		_width = rand( FlxG.width );
+		_height = rand( 10 );
 		
-		_width = FlxRandom.intRanged( 0, FlxG.width );
-		_height = FlxRandom.intRanged( 0, 10 );
-		
-		_rectangle = new Rectangle( _x, _y, _width, _height );
-		generateRandomRectangle();
+		_rectangle = new Rectangle( rand( FlxG.width ), rand( FlxG.height ) , _width, _height );
+		_randomRectangle = generateRandomRectangle();
 		
 		_dead = FlxRandom.chanceRoll();
-		_type = FlxRandom.intRanged( 0, 2 );
-		
-		_hospitalWithout = new FlxSprite( 0, 0, "images/hospital_without.png" );
 	}
 	
+	/**
+	 * Applies this glitch to the camera.
+	 */
 	public function execute():Void
 	{
-		switch( _type ) {
-			case 0:
-				if ( !_dead )
-					generateRandomRectangle();
-				_vector = FlxG.camera.buffer.getVector( _randomRectangle );
-				FlxG.camera.buffer.setVector( _rectangle, _vector );
-				_once = true;
-			case 1:
-				_vector = _hospitalWithout.framePixels.getVector( _randomRectangle );
-				FlxG.camera.buffer.setVector( _rectangle, _vector );
-			case 2:
-				_vector = _hospitalWithout.framePixels.getVector( _rectangle );
-				FlxG.camera.buffer.setVector( _rectangle, _vector );
-		}
+		if ( !_dead )
+			_randomRectangle = generateRandomRectangle();
+		
+		_vector = FlxG.camera.buffer.getVector( _randomRectangle );
+		FlxG.camera.buffer.setVector( _rectangle, _vector );
 	}
 	
-	private function generateRandomRectangle():Void
+	/**
+	 * Generates a random rectangle.
+	 */
+	private function generateRandomRectangle():Rectangle
 	{
-		_randomRectangle = new Rectangle( FlxRandom.intRanged( 0, Std.int( FlxG.width - _width ) ), FlxRandom.intRanged( 0, Std.int( FlxG.height - _height ) ), _width, _height );
+		return new Rectangle( rand( FlxG.width - _width ), rand( FlxG.height - _height ), _width, _height );
+	}
+	
+	/**
+	 * Generate a random integer between 0 and Max. Just shorthand for FlxRandom.intRanged
+	 */
+	private function rand( Max:Int ):Int
+	{
+		return FlxRandom.intRanged( 0, Max );
 	}
 }
